bb7ed385a9608c378e92b832b36ea6bf83484f37
Ninad <telangn@gmail.com>
2017-11-22 09:57:04 -0800

    cleaned up code

--- RestAssuredCucumber/src/test/java/APITesting/RestAssuredCucumber/StepDefinitions.java
+++ RestAssuredCucumber/src/test/java/APITesting/RestAssuredCucumber/StepDefinitions.java
@@ -22,12 +22,9 @@ import junit.framework.Assert;
 public class StepDefinitions {
 
 	/**
-	 * given() 
-	 * set cookies, add authorization, add parameters, setting header info 
-	 * when()
-	 * GET, POST, PUT, DELETE..etc 
-	 * then() 
-	 * validate status code, extract response, extract headers, cookies, body
+	 * given() set cookies, add authorization, add parameters, setting header info
+	 * when() GET, POST, PUT, DELETE..etc 
+	 * then() validate status code, extract, response, extract headers, cookies, body
 	 */
 
 	public Response response;
@@ -40,10 +37,9 @@ public class StepDefinitions {
 	public void endPointUp(String arg1) throws Throwable {
 
 		URL = arg1;
-		response = 	given()
-					.when()
-						.get(URL);
-		
+
+		response = given().when().get(URL);
+
 		Assert.assertEquals(200, response.getStatusCode());
 	}
 
@@ -51,7 +47,6 @@ public class StepDefinitions {
 	public void requestURL(String arg1) throws Throwable {
 		URL = URL + arg1;
 	}
-	
 
 	@When("^I want to (?:patch|put|delete) data on id \"([^\"]*)\"$")
 	public void updateOnEndPoint(String arg1) throws Throwable {
@@ -62,64 +57,54 @@ public class StepDefinitions {
 	public void parameters(DataTable arg1) throws Throwable {
 
 		java.util.List<java.util.List<String>> data = arg1.raw();
-		request = 
-				given()
-					.param(data.get(0).get(0), data.get(0).get(1))
-					.param(data.get(1).get(0), data.get(1).get(1));
+
+		request = given()
+				.param(data.get(0).get(0), data.get(0).get(1))
+				.param(data.get(1).get(0), data.get(1).get(1));
 	}
 
 	@When("^I want to make a post with data$")
 	public void postWithData(DataTable arg1) throws Throwable {
-		
+
 		java.util.List<java.util.List<String>> data = arg1.raw();
-		
+
 		Info info = new Info();
 		info.setPublisher(data.get(3).get(1));
 		info.setIsbn(data.get(4).get(1));
 		info.setCatalogNumber(data.get(5).get(1));
-		
+
 		Posts newPost = new Posts();
 		newPost.setId(data.get(0).get(1));
 		newPost.setTitle(data.get(1).get(1));
 		newPost.setAuthor(data.get(2).get(1));
-		newPost.setInfo(new Info[] {info});
+		newPost.setInfo(new Info[] { info });
 
-		request = 
-				given()
-				.when()
-					.body(newPost)
-					.contentType(ContentType.JSON);
+		request = given().when().body(newPost).contentType(ContentType.JSON);
 	}
-	
+
 	@When("^I want to (?:patch|put) with data$")
 	public void putWithData(DataTable arg1) throws Throwable {
-		
+
 		java.util.List<java.util.List<String>> data = arg1.raw();
-		
+
 		Posts newPost = new Posts();
 		newPost.setTitle(data.get(0).get(1));
 		newPost.setAuthor(data.get(1).get(1));
-		
-		request = 
-				given()
-				.when()
-					.body(newPost)
-					.contentType(ContentType.JSON);
-		
+
+		request = given().when().body(newPost).contentType(ContentType.JSON);
+
 	}
 
 	@Then("^the weather description is \"([^\"]*)\"$")
 	public void weatherDescription(String arg1) throws Throwable {
 
 		response = request.get(URL);
-		System.out.println(response.asString());
-		String weatherDescription = 
-				response
-					.then()
-						.contentType(ContentType.JSON)
-						.extract()
-						.path("weather[0].description");
-		
+
+		String weatherDescription = response.then()
+				.contentType(ContentType.JSON)
+				.extract()
+				.path("weather[0].description");
+
 		Assert.assertEquals(arg1, weatherDescription);
 
 	}
@@ -138,14 +123,12 @@ public class StepDefinitions {
 		 */
 
 		response = request.get(URL);
-		String actualValue = 
-				response
-					.then()
-						.contentType(ContentType.JSON)
-						.extract()
-						.path(arg1);
-		
-		System.out.println(actualValue);
+
+		String actualValue = response.then()
+				.contentType(ContentType.JSON)
+				.extract()
+				.path(arg1);
+
 		Assert.assertEquals(arg2, actualValue);
 
 	}
@@ -155,25 +138,23 @@ public class StepDefinitions {
 
 		response = request.post(URL);
 	}
-	
+
 	@Then("^I make the put$")
 	public void put() throws Throwable {
-		
+
 		response = request.put(URL);
 	}
-	
+
 	@Then("^I make the patch$")
 	public void patch() throws Throwable {
-		
+
 		response = request.patch(URL);
 	}
-	
+
 	@Then("^I make the delete$")
 	public void delete() throws Throwable {
-		
-		response = 	given()
-					.when()
-						.delete(URL);
+
+		response = given().when().delete(URL);
 	}
 
 }
