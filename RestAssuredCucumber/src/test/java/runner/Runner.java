326b917b65d203ef02d4c65f4b66712b47a0c186
Ninad <telangn@gmail.com>
2018-08-13 20:45:11 -0700

    cleaned up hamcrest

--- RestAssuredCucumber/src/test/java/runner/Runner.java
+++ RestAssuredCucumber/src/test/java/runner/Runner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(format = { "pretty", "html:/home/ninad/eclipse-workspace/Reports/html/",
 		"json:/home/ninad/eclipse-workspace/Reports/cucumber-report.json" }, features = {
-				"src/test/resource" }, glue = "stepDefinitions", tags = { "@jsonServer2" })
+				"src/test/resource" }, glue = {"stepDefinitions"}, tags = { "@jsonServer2" })
 public class Runner {
 
 }
